86f71acf3b73b7739080aedcd5b3f6a7e7f4c609
betin <betinvoker1797@gmail.com>
2025-10-09 00:38:23 +0300

    Start lab3_6

---
+++ 3_6/train.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPRegressor
+
+X_train = np.random.uniform(-6,6, size=(500,1))
+y_train = np.cos(X_train)
+
+X_test = np.random.uniform(-6,6, size=(500,1))
+y_test = np.cos(X_test)
+
+plt.figure(figsize=(8,5))
+plt.scatter(X_train, y_train, label='train')
+plt.scatter(X_test, y_test, label='test')
+plt.legend()
+plt.show()
